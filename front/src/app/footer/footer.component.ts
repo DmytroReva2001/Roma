a42f1e4ed5cd5a67f5d7d1c98d7a596d50b05e14
Dmytro Reva <dmytroreva2001@gmail.com>
2024-09-17 15:04:13 +0200

    Mejorado desplazamiento de footer

--- front/src/app/footer/footer.component.ts
+++ front/src/app/footer/footer.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked } from '@angular/core';
 import { InfoService } from '../services/info.service';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewChecked {
   // Variable para guardar info
   informacion: any = {};
-  
+
   constructor(
     private infoService: InfoService,
+    private router: Router
   ) {
     // Suscribimos al response de servicio
     this.infoService.getInformacion().subscribe(datos => {
@@ -19,5 +22,19 @@ export class FooterComponent {
         this.informacion[item.dato] = item.valor;
       });
     });
+
+    // Suscribirse a NavigationEnd para desplazar a la parte superior después de la navegación
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      setTimeout(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }, 100); // Retraso opcional para asegurar que la página se haya cargado completamente
+    });
+  }
+
+  // Método para asegurar que la página se desplaza hacia arriba
+  ngAfterViewChecked(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
